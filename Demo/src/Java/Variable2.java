38525f213e67d6947604eed97caf1899158894c3
ImpactGuru <ImpactGuru@User>
2024-08-14 17:12:26 +0530

    Second practice code

---
+++ Demo/src/Java/Variable2.java
@@ -0,0 +1,26 @@
+package Java;
+
+public class Variable2 {
+
+	public static void main(String[] args) {
+		//variable Declared
+		String employeename;
+		int rollno;
+		float percentage;
+		char grade;
+		
+		//Variable initialise
+		employeename = "Deep";
+		rollno = 2779;
+		percentage = 22.39f;
+		grade = 'A';
+		
+		//Variable use
+		System.out.println(employeename);
+		System.out.println(rollno);
+		System.out.println(percentage);
+		System.out.println(grade);
+
+	}
+
+}
