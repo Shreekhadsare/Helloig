1f75d4af945429d8146694dc3b8ec8e9f7ccabc0
ImpactGuru <ImpactGuru@User>
2024-08-14 16:27:27 +0530

    14 aug variable

---
+++ Demo/src/Java/Variable.java
@@ -0,0 +1,27 @@
+package Java;
+
+public class Variable {
+	public static void main(String[] args) 
+	{
+		//variable declaration
+		String studentnames;
+		int studentRollNum;
+		char studentgrade;
+		float studentpercentage;
+		
+		//Variable intialisation
+		studentnames="shree"+" "+"Khadsare";
+		studentRollNum= 101;
+		studentgrade = 'c';
+		studentpercentage = 22.3f;
+		
+		
+		//Variable use
+		System.out.println(studentnames);
+		System.out.println(studentRollNum);
+		System.out.println(studentgrade);
+		System.out.println(studentpercentage);
+		
+	}
+
+}
